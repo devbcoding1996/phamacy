88f719914cec76d1a5d87e03120f0599a655d6b4
weedanai luamtaisong <weedanai.lua@thailife.com>
2023-12-21 00:15:36 +0700

    manage address

--- FrontEnd/js/customer.js
+++ FrontEnd/js/customer.js
@@ -14,22 +14,22 @@ customerList = async (id) => {
   await fetch(`https://api.wakeupcoding.com/pharmacy-api/customer/list/${id}`, requestOptions)
     .then(async (result) => {
       let response = await result.json();
-      console.log("response", response);
       if (response) {
-        localStorage.setItem("customer_id", response.customer.customer_id);
+        localStorage.setItem("customer_id", response.customer.id);
         document.getElementById("top-account").innerHTML = "";
         document.getElementById("top-account").innerHTML = `
-            <div class="dropdown">
-                <a href="#" class="dropdown-toggle" role="button" id="hasLogin"
-                    data-bs-toggle="dropdown" aria-expanded="false">
-                    คุณ ${response.customer.f_name} ${response.customer.l_name}
-                </a>
+          <div class="dropdown">
+              <a href="#" class="dropdown-toggle" role="button" id="hasLogin"
+                  data-bs-toggle="dropdown" aria-expanded="false">
+                  คุณ ${response.customer.fName} ${response.customer.lName}
+              </a>
 
-                <ul class="dropdown-menu" aria-labelledby="hasLogin">
-                    <li><a class="dropdown-item" href="history.html">ประวัติคำสั่งซื้อ</a></li>
-                    <li><a class="dropdown-item" href="#" onclick="logout()">ออกจากระบบ</a></li>
-                </ul>
-            </div>
+              <ul class="dropdown-menu" aria-labelledby="hasLogin">
+                  <li><a class="dropdown-item" href="history.html">ประวัติคำสั่งซื้อ</a></li>
+                  <li><a class="dropdown-item" href="address.html">จัดการที่อยู่</a></li>
+                  <li><a class="dropdown-item" href="#" onclick="logout()">ออกจากระบบ</a></li>
+              </ul>
+          </div>
         `;
       }
     })
@@ -54,7 +54,6 @@ callListCustomer = async () => {
     await fetch("https://api.wakeupcoding.com/pharmacy-api/userCustomer/listCustomerId", requestOptions)
       .then(async (result) => {
         let response = await result.json();
-        console.log("response", response);
         if (response) {
           localStorage.setItem("customer_id", response.userCustomer.customer_id);
           customerList(response.userCustomer.customer_id);
@@ -62,6 +61,13 @@ callListCustomer = async () => {
       })
       .catch((error) => {
         console.log("error", error);
+        document.getElementById("top-account").innerHTML = "";
+        document.getElementById("top-account").innerHTML = `
+        <a href="#modal-register" id="noLogin" data-lightbox="inline">
+            <i class="bi-person me-1 position-relative" style="top: 1px;"></i>
+            <span class="d-none d-sm-inline-block font-primary fw-medium">เข้าสู่ระบบ</span>
+        </a>
+    `;
       });
   } else {
     document.getElementById("top-account").innerHTML = "";
--- FrontEnd/js/payment.js
+++ FrontEnd/js/payment.js
@@ -22,6 +22,7 @@
   });
 })();
 
+var _total = 0;
 orDerDetailList = async (orderId) => {
   var myHeaders = new Headers();
   let token = localStorage.getItem("token");
@@ -40,7 +41,6 @@ orDerDetailList = async (orderId) => {
       let output = "";
       let total = 0;
       let no = 1;
-      console.log("response", response.orderDrugDetail);
       response.orderDrugDetail.forEach((element) => {
         output += `
           <li class="list-group-item d-flex justify-content-between lh-sm">
@@ -53,6 +53,7 @@ orDerDetailList = async (orderId) => {
         total += element.total;
         no++;
       });
+      _total = total;
       $(".order-code").html(orderId);
       $(".show-cart").html(output);
       $(".total-cart").html(`${total.toFixed(2)}฿`);
@@ -101,7 +102,44 @@ customerCreate = async () => {
   await fetch("https://api.wakeupcoding.com/pharmacy-api/customer/create", requestOptions)
     .then(async (result) => {
       let response = await result.json();
-      console.log("response", response);
+      if (response) {
+        orderDrugUpdate(response.customerId);
+      }
+    })
+    .catch((error) => {
+      console.log("error", error);
+      Swal.fire({
+        icon: "error",
+        title: "ผิดพลาด",
+        text: error.error,
+      });
+    });
+};
+
+orderDrugUpdate = async (customerId) => {
+  var myHeaders = new Headers();
+  let token = localStorage.getItem("token");
+  let orderId = localStorage.getItem("orderId");
+  myHeaders.append("Content-Type", "application/json");
+  myHeaders.append("Authorization", `Bearer ${token}`);
+
+  var raw = JSON.stringify({
+    id: orderId,
+    customerId: customerId,
+    total: _total,
+    status: "WP",
+  });
+
+  var requestOptions = {
+    method: "PUT",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  await fetch("https://api.wakeupcoding.com/pharmacy-api/orderDrug/update/", requestOptions)
+    .then(async (result) => {
+      let response = await result.json();
       if (response.message) {
         document.getElementById("load").style.display = "none";
         Swal.fire({
@@ -122,3 +160,46 @@ customerCreate = async () => {
       });
     });
 };
+
+handleAddress = async (e) => {
+  if (e === "new") {
+    $("#firstName").val("");
+    $("#lastName").val("");
+    $("#address").val("");
+    $("#phonenumber").val("");
+    $("#email").val("");
+  } else {
+    var myHeaders = new Headers();
+    let token = localStorage.getItem("token");
+    let cusId = localStorage.getItem("customer_id");
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Authorization", `Bearer ${token}`);
+
+    var requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow",
+    };
+
+    await fetch("https://api.wakeupcoding.com/pharmacy-api/customer/list/" + cusId, requestOptions)
+      .then(async (result) => {
+        let response = await result.json();
+        let data = response.customer;
+        if (response) {
+          $("#firstName").val(data.fName);
+          $("#lastName").val(data.lName);
+          $("#address").val(data.address);
+          $("#phonenumber").val(data.phoneNumber);
+          $("#email").val(data.email);
+        }
+      })
+      .catch((error) => {
+        Swal.fire({
+          icon: "error",
+          title: "ผิดพลาด",
+          text: error.error,
+        });
+      });
+  }
+};
+handleAddress("new");
